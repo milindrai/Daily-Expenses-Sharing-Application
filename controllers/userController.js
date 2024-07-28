116b338c16746714a0e14eeadce20f803c79f2e9
milindrai <milindrai365@gmail.com>
2024-07-28 14:57:10 +0530

    add getUserDetails endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,3 +12,12 @@ exports.createUser = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+exports.getUserDetails = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
