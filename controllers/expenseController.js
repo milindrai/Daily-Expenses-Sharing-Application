50e1b042d863db2b6691cc90c35222f1eee0e81c
milindrai <milindrai365@gmail.com>
2024-07-28 15:10:22 +0530

    Add expense controller

---
+++ controllers/expenseController.js
@@ -0,0 +1,40 @@
+const Expense = require('../models/expense');
+
+exports.addExpense = async (req, res) => {
+    const { description, amount, splitMethod, participants } = req.body;
+    try {
+        const expense = new Expense({ description, amount, splitMethod, participants, createdBy: req.user.id });
+        await expense.save();
+        res.status(201).json(expense);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getIndividualExpenses = async (req, res) => {
+    try {
+        const expenses = await Expense.find({ 'participants.userId': req.user.id });
+        res.status(200).json(expenses);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getOverallExpenses = async (req, res) => {
+    try {
+        const expenses = await Expense.find({});
+        res.status(200).json(expenses);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.downloadBalanceSheet = async (req, res) => {
+    try {
+        const expenses = await Expense.find({ 'participants.userId': req.user.id });
+        const balanceSheet = generateBalanceSheet(expenses);
+        res.status(200).json(balanceSheet);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
